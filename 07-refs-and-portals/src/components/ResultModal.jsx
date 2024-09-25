b9a51fd9680a08c5383fc9124172c9a9b7494070
selmasaltik <selmasaltiik@gmail.com>
2024-09-25 15:48:08 +0300

    Revert "refs and portals"
    This reverts commit 7893a4c899302f941b79f6ff33f47f13b3ef980d.

--- 07-refs-and-portals/src/components/ResultModal.jsx
+++ 07-refs-and-portals/src/components/ResultModal.jsx
@@ -1,18 +1,8 @@
-import { forwardRef, useImperativeHandle, useRef } from "react";
+import { forwardRef } from "react";
 
 const ResultModal = forwardRef(function ResultModal({result, targetTime}, ref) {
-    const dialog = useRef();
-
-    useImperativeHandle(ref, () => {
-        return {
-            open() {
-                dialog.current.showModal();
-            }
-        };
-    });
-
     return(
-        <dialog ref={dialog} className="result-modal">
+        <dialog ref={ref} className="result-modal">
             <h2>You {result}!</h2>
             <p>
                 The target time was <strong>{targetTime} seconds.</strong>
